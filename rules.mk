5982fa0ffc4ce91885df6be3ef9eda45bd9b1153
Stephen Robinson <stephen.robinson@1st-edge.com>
2023-12-15 12:21:49 -0600

    Made changes to work with newest QMK version(s)

--- rules.mk
+++ rules.mk
@@ -1,5 +1,4 @@
 OLED_ENABLE = yes   # Enables the use of OLED displays
-OLED_DRIVER = SSD1306
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 MOUSEKEY_ENABLE = no
